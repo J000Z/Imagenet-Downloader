ada6dba61e5339e910787481cd73e1d6a51b80c9
cheng9393 <zhujiancheng@outlook.com>
2016-09-23 08:33:08 -0400

    add additional debug output

--- downloader.py
+++ downloader.py
@@ -67,7 +67,7 @@ def worker(cur, cur_lock, collection, run_event, info):
             info['total'],
             info['count']/info['total']*100))
         info['count'] += 1
-    logging.debug('stop')
+    logging.debug('stop with run_event={}'.format(run_event.is_set()))
 
 client = MongoClient()
 collection = client.imagenet.urls
