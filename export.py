1fa9e15713a7682988aaeac7a15871032f755d95
cheng9393 <zhujiancheng@outlook.com>
2016-10-10 15:12:33 -0400

    add export

---
+++ export.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from pymongo import MongoClient
+from io import BytesIO
+from urlparse import urlparse
+import os
+from bson.binary import Binary
+import logging
+
+
+client = MongoClient()
+collection = client.imagenet.urls
+cursor = collection.find({'data': {'$ne': None}}).sort("_id")
+folder = sys.argv[1]
+
+info = {'count': 0., 'total': 14197121.}
+row = None
+while True:
+    try:
+        info['count'] += 1
+        row = cursor.next()
+    except StopIteration:
+        logging.debug('StopIteration')
+        break
+    url = row['url']
+    id_ = row['id']
+    data = row['data']
+
+    # get extension
+    ext = os.path.splitext(urlparse(url).path)[1]
+    # construct file name
+    filename = "{}/{}{}".format(folder, id_, ext)
+
+    with open(filename, 'wb+') as f:
+        f.write(data)
+
+    if info['count'] % 1000 == 0:
+        progress = info['count']/info['total']*100
+        logging.debug('progress {}/{} {}%'.format(
+            info['count'],
+            info['total'],
+            progress))
