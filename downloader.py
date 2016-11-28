95ab8537dcac0c595028acbc9c07bfbfa6369e46
cheng9393 <zhujiancheng@outlook.com>
2016-11-28 11:06:55 -0500

    rob and queue

--- downloader.py
+++ downloader.py
@@ -2,7 +2,6 @@
 
 import threading
 import logging
-from pymongo import MongoClient
 import requests
 from io import BytesIO
 from bson.binary import Binary
@@ -12,8 +11,15 @@ from requests.adapters import HTTPAdapter
 from datadog import statsd
 from datadog.api.constants import CheckStatus
 import random
+import shelve
+from collections import deque
+from queue import FifoSQLiteQueue
 
-user_agents = [
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] [%(levelname)s] (%(threadName)-10s) %(message)s')
+
+USER_AGENTS = [
     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36',
     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
@@ -36,126 +42,250 @@ user_agents = [
     'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
 ]
 
-s = requests.Session()
-s.mount('http://', HTTPAdapter(max_retries=1))
-s.mount('https://', HTTPAdapter(max_retries=1))
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='[%(asctime)s] [%(levelname)s] (%(threadName)-10s) %(message)s')
+class SourceCursor(object):
 
-info = {'count': 0., 'total': 14197121.}
+    _fields = 'ImageID, OriginalURL'
+    _sql_first = 'SELECT {} FROM urls ORDER BY ImageID ASC limit 1'.format(_fields)
+    _sql_move_to = 'SELECT {} FROM urls WHERE ImageID>? ORDER BY ImageID ASC'.format(_fields)
+    _sql_size = 'SELECT COUNT(*) FROM queue'
+
+    def synchronized(func):
+        def func_wrapper(*args, **kwargs):
+            self.mutex.acquire()
+            result = func(*args, **kwargs)
+            self.mutex.release()
+            return result
+        return func_wrapper
+
+    def __init__(self, path, last_id=None):
+        self._path = os.path.abspath(path)
+        self._db = sqlite3.Connection(self._path, timeout=60)
+        self.mutex = Lock()
+
+    def initialCursor(last_id=None):
+        if last_id is None:
+            self.cursor = conn.execute(self._sql_first)
+        else:
+            self.cursor = conn.execute(self._sql_move_to, (last_id,))
+
+    @synchronized
+    def next(self):
+        return self.cursor.next()
+
+    @synchronized
+    def total(self):
+        with self._db as conn:
+            return next(conn.execute(self._sql_size))[0]
 
 
-def process(row, collection, retry=0):
-    id_, url = row.split()
-    item = collection.find_one({"id": id_})
-    if 'status' in item and item['status'] != -1:
-        logging.debug('{} {}'.format(id_, "skip with status"))
-        return
+class Config(object):
+
+    CONFIG_LAST_ID = 'LAST_ID'
+    CONFIG_PROCESSED_COUNT = 'PROCESSED_COUNT'
+    CONFIG_TOTAL = 'TOTAL'
+
+    def synchronized(func):
+        def func_wrapper(*args, **kwargs):
+            self.mutex.acquire()
+            result = func(*args, **kwargs)
+            self.mutex.release()
+            return result
+        return func_wrapper
+
+    def __init__(self, filename):
+        super(Configuration, self).__init__()
+        self.mutex = Lock()
+        self.shelve = shelve.open(filename)
+
+    @synchronized
+    def __getitem__(self, key):
+        return self.shelve.__getitem__(key)
+
+    @synchronized
+    def __setitem__(self, key, value):
+        self.shelve.__setitem__(key, value)
+        self.shelve.sync()
+
+    @synchronized
+    def __contains__(self, key):
+        return self.shelve.__contains__(key)
+
+    @synchronized
+    def inc(self, key):
+        if key in self.shelve:
+            self.shelve[key] += 1
+        else:
+            self.shelve[key] = 1.0
+        self.shelve.sync()
+        self.logProgress()
+
+    @synchronized
+    def logProgress(self):
+        progress = self.shelve[CONFIG_PROCESSED_COUNT] / self.shelve[CONFIG_TOTAL] * 100
+        logging.debug('progress {}/{} {}%'.format(
+            self.shelve[CONFIG_PROCESSED_COUNT],
+            self.shelve[CONFIG_TOTAL],
+            progress))
+        statsd.gauge('url.downloader.progress', progress)
+
+    @synchronized
+    def close():
+        self.shelve.close()
+
+
+class ROB(object):
+
+    def synchronized(func):
+        def func_wrapper(*args, **kwargs):
+            self.mutex.acquire()
+            result = func(*args, **kwargs)
+            self.mutex.release()
+            return result
+        return func_wrapper
+
+    def __init__(self, output_queue, config):
+        self.q = deque()
+        self.d = {}
+        self.o = output_queue
+        self.c = config
+        self.mutex = Lock()
+
+    @synchronized
+    def pending(self, id_):
+        self.q.append(id_)
+
+    @synchronized
+    def push(self, id_, obj):
+        self.d[id_] = obj
+        self.check()
+
+    def check(self):
+        if len(self.q) == 0:
+            return
+        id_ = self.q[0]
+        if id_ in self.d:
+            success, data = self.d[id_]
+            if success:
+                self.o.push(data)
+            self.c.inc(Config.CONFIG_PROCESSED_COUNT)
+            self.c[Config.CONFIG_LAST_ID] = id_
+            del self.d[id_]
+            self.q.popleft()
+            self.check()
+
+
+def init_requests():
+    s = requests.Session()
+    s.mount('http://', HTTPAdapter(max_retries=1))
+    s.mount('https://', HTTPAdapter(max_retries=1))
+
+
+def fetch(url, retry=0):
     if retry == 2:
         statsd.increment('url.failed')
-        collection.update_one({'id': id_},
-                              {"$set": {'data': None,
-                                        'status': -2}})
         logging.debug('{} {}'.format(id_, "max retry"))
-        return
+        return (False, None)
     try:
-
         r = requests.get(url,
                          timeout=1,
-                         headers={'User-Agent': random.choice(user_agents)})
+                         headers={'User-Agent': random.choice(USER_AGENTS)})
         if (r.status_code == 200):
-            b = Binary(BytesIO(r.content).getvalue())
-            collection.update_one({'id': id_},
-                                  {"$set": {'data': b,
-                                            'status': 200}})
+            b = r.content.getvalues()
             logging.debug('{} {}'.format(id_, 'done'))
             statsd.increment('url.success')
+            return (True, b)
         else:
             statsd.increment('url.failed')
-            collection.update_one({'id': id_},
-                                  {"$set": {'data': None,
-                                            'status': r.status_code}})
             logging.debug('{} {}'.format(id_, 'failed'))
+            return (False, None)
     except requests.exceptions.ConnectionError:
         time.sleep(60)
-        process(row, collection, retry+1)
+        process(url, retry + 1)
     except Exception as e:
         statsd.increment('url.failed')
-        # collection.update_one({'id': id_},
-        #                      {"$set": {'data': None,
-        #                                'status': -1}})
         logging.debug('{} {}'.format(id_, str(e)))
+        return (False, None)
 
 
-def worker(cur, cur_lock, collection, run_event, info):
+def worker(cur, cursor_lock, run_event, rob, queue):
     logging.debug('start')
-    row = None
     while run_event.is_set():
-        with cursor_lock:
-            info['count'] += 1
-            try:
-                row = cur.next()
-            except StopIteration:
-                logging.debug('StopIteration')
-                break
+        if len(queue) >= 5000:
+            time.sleep(5)
+            continue
+        try:
+            with cursor_lock:
+                id_, url = cur.next()
+                rob.pending(id_)
+        except StopIteration:
+            logging.debug('StopIteration')
+            break
         statsd.increment('url.process')
-        process(row, collection)
-        with cursor_lock:
-            progress = info['count']/info['total']*100
-            logging.debug('progress {}/{} {}%'.format(
-                info['count'],
-                info['total'],
-                progress))
-            statsd.gauge('url.downloader.progress', progress)
+        result = fetch(url)
+        rob.push(id_, result)
     logging.debug('stop with run_event={}'.format(run_event.is_set()))
 
-client = MongoClient(maxPoolSize=None)
-collection = client.imagenet.urls
-cursor = open('/root/imagenet/fall11_urls.txt', 'r')
-# cursor = collection.find({'status': {'$exists': False}}).sort("_id")
-cursor_lock = threading.Lock()
-skip = int(sys.argv[2])
-info['count'] = skip
-logging.debug('skip {}'.format(skip))
-for _ in xrange(skip):
-    cursor.next()
-run_event = threading.Event()
-run_event.set()
 
+def main():
+    parser = argparse.ArgumentParser(description='urls downloader')
+    parser.add_argument('config_path', help='The config path (will be created if not exists)')
+    parser.add_argument('urls_path', help='The urls db path')
+    parser.add_argument('queue_path', help='The buffer queue path')
+    args = parser.parse_args()
 
-def genThread(i):
-    return threading.Thread(name='worker_{}'.format(i),
-                            target=worker,
-                            args=(cursor,
-                                  cursor_lock,
-                                  collection,
-                                  run_event,
-                                  info, ))
-threads = {}
-threads_count = int(sys.argv[1])
-for i in range(threads_count):
-    threads[i] = genThread(i)
-    threads[i].start()
-    time.sleep(0.5)
+    init_requests()
 
-try:
-    while 1:
-        time.sleep(5)
-        statsd.service_check(check_name='url.downloader',
-                             status=CheckStatus.OK,
-                             message='heart beat ok')
-        for k, t in threads.items():
-            if not t.isAlive():
-                threads[k] = genThread(k)
-                threads[k].start()
-except KeyboardInterrupt:
-    logging.debug('attempting to close threads')
-    run_event.clear()
-    for t in threads.values():
-        t.join()
-    logging.debug('threads successfully closed')
+    config = Config(args.config_path)
+    queue = FifoSQLiteQueue(args.queue_path)
+    rob = ROB(queue, config)
+    cursor = None
+    if Config.CONFIG_TOTAL in config:  # has config
+        cursor = SourceCursor(args.urls_path, config[Config.CONFIG_LAST_ID])
+        logging.debug('resume from {}, with {} done'.format(
+            config[Config.CONFIG_LAST_ID]
+            config[Config.CONFIG_PROCESSED_COUNT]))
+    else:  # no config
+        cursor = SourceCursor(args.urls_path)
+        config[Config.CONFIG_TOTAL] = cursor.total()
+        config[Config.CONFIG_PROCESSED_COUNT] = 0.0
+        logging.debug('new Job')
 
-cursor.close()
-logging.debug('all done')
+    cursor_lock = threading.Lock()
+    run_event = threading.Event()
+    run_event.set()
+
+    threads = {}
+    threads_count = int(sys.argv[1])
+    for i in range(threads_count):
+        threads[i] = threading.Thread(name='worker_{}'.format(i),
+                                      target=worker,
+                                      args=(cursor,
+                                            cursor_lock,
+                                            run_event,
+                                            rob,
+                                            queue, ))
+        threads[i].start()
+        time.sleep(0.5)
+
+    try:
+        while 1:
+            time.sleep(5)
+            statsd.service_check(check_name='url.downloader',
+                                 status=CheckStatus.OK,
+                                 message='heart beat ok')
+            for k, t in threads.items():
+                if not t.isAlive():
+                    threads[k] = genThread(k)
+                    threads[k].start()
+    except KeyboardInterrupt:
+        logging.debug('attempting to close threads')
+        run_event.clear()
+        for t in threads.values():
+            t.join()
+        logging.debug('threads successfully closed')
+
+    cursor.close()
+    queue.close()
+    config.close()
+    logging.debug('all done')
---
+++ queue.py
@@ -0,0 +1,87 @@
+import sqlite3
+from threading import Lock
+
+
+class FifoSQLiteQueue(object):
+
+    _sql_create = (
+        'CREATE TABLE IF NOT EXISTS queue '
+        '(id INTEGER PRIMARY KEY AUTOINCREMENT, item BLOB)'
+    )
+    _sql_size = 'SELECT COUNT(*) FROM queue'
+    _sql_push = 'INSERT INTO queue (item) VALUES (?)'
+    _sql_pop = 'SELECT id, item FROM queue ORDER BY id LIMIT 1'
+    _sql_del = 'DELETE FROM queue WHERE id = ?'
+    _sql_get = 'SELECT id, item FROM queue WHERE id = ?'
+
+    def synchronized(func):
+        def func_wrapper(*args, **kwargs):
+            self.mutex.acquire()
+            result = func(*args, **kwargs)
+            self.mutex.release()
+            return result
+        return func_wrapper
+
+    def __init__(self, path):
+        self._path = os.path.abspath(path)
+        self._db = sqlite3.Connection(self._path, timeout=60)
+        self._db.text_factory = bytes
+        self.mutex = Lock()
+        with self._db as conn:
+            conn.execute(self._sql_create)
+        self.size = self.size()
+
+    @synchronized
+    def push(self, item):
+        if not isinstance(item, bytes):
+            raise TypeError('Unsupported type: {}'.format(type(item).__name__))
+
+        with self._db as conn:
+            conn.execute(self._sql_push, (item,))
+        self.size += 1
+
+    # @synchronized
+    # def pop(self):
+    #     with self._db as conn:
+    #         for id_, item in conn.execute(self._sql_pop):
+    #             conn.execute(self._sql_del, (id_,))
+    #             return (id_, item)
+    #         conn.execute(self._sql_del, (id_,))
+
+    @synchronized
+    def peek(self, id_=None):
+        with self._db as conn:
+            if id_ is None:
+                for result in conn.execute(self._sql_pop):
+                    return result
+            else:
+                for result in conn.execute(self._sql_get, (id_,)):
+                    return result
+
+    @synchronized
+    def delete(self, id_):
+        with self._db as conn:
+            conn.execute(self._sql_del, (id_,))
+            self.size -= 1
+
+    @synchronized
+    def close(self):
+        size = len(self)
+        self._db.close()
+        if not size:
+            os.remove(self._path)
+
+    @synchronized
+    def size(self):
+        with self._db as conn:
+            return next(conn.execute(self._sql_size))[0]
+
+    def __getitem__(self, id_):
+        return self.peek(id_)
+
+    def __delitem__(self, id_):
+        self.delete(id_)
+
+    @synchronized
+    def __len__(self):
+        return self.size
